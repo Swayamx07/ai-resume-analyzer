4c98b185ed225c256d08b078b370563c11eb28d9
Swayamx07 <patilswayam1907@gmail.com>
2026-01-22 23:45:12 +0530

    Added analysis API endpoint

--- index.js
+++ index.js
@@ -7,12 +7,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = 5000;
+
 // health check route
 app.get("/health", (req, res) => {
     res.json({ status: "Server running" });
 });
 
-const PORT = 5000;
+const jobRoles = require("./data/jobRoles");
+const calculateScore = require("./utils/scoreCalculator");
+
+app.post("/api/analyze", (req, res) => {
+
+    const resumeSkills = ["javascript", "react", "html", "css"];
+
+    const { role } = req.body;
+
+    const selectedRole = jobRoles.find(
+        job => job.role === role
+    );
+
+    if (!selectedRole) {
+        return res.status(404).json({ message: "Role not found" });
+    }
+
+    const result = calculateScore(resumeSkills, selectedRole);
+
+    res.json({
+        role: selectedRole.role,
+        ...result
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
