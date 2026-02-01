905cf5df2f32fbfd0e88ef7b3c2bfd2b6fd2fb83
Swayamx07 <patilswayam1907@gmail.com>
2026-02-01 10:57:37 +0530

    Configured Tailwind CSS v4 with Vite plugin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import Layout from "./components/Layout";
-import ResumeUpload from "./components/ResumeUpload";
+// import Layout from "./components/Layout";
+// import ResumeUpload from "./components/ResumeUpload";
+
+// function App() {
+//   return (
+//     <Layout>
+//       <ResumeUpload />
+//     </Layout>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <Layout>
-      <ResumeUpload />
-    </Layout>
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Tailwind v4 Working 🚀
+      </h1>
+    </div>
   );
 }
 
 export default App;
+
